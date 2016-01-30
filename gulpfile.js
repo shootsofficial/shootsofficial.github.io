d0c8f152cbe341d1589f8b263d0a3683048d3f9d
Jason Hargrove <jason@objas.com>
2016-01-30 15:55:59 -0500

    Update 2016-01-30T20:55:57.881Z

--- gulpfile.js
+++ gulpfile.js
@@ -36,7 +36,7 @@ gulp.task('lint', function () {
 });
 
 // Deploy to production
-gulp.task('deploy:production', ['build'], function() {
+gulp.task('deploy:production', ['build', 'kraken'], function() {
   return gulp.src('./_site/**/*')
     .pipe(deploy({
       remoteUrl: 'https://github.com/shootsofficial/shootsofficial.github.io',
